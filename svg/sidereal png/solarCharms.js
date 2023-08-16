14ccc99b000d715493d95352df322f3c18a16710
Nick H <113550184+NicKreb@users.noreply.github.com>
2023-08-15 21:55:24 -0500

    Create solarCharms.js

---
+++ svg/sidereal png/solarCharms.js
@@ -0,0 +1,114 @@
+let solarCharm = [
+  {
+    title: "There is No Wind",
+    statMin: "Archery 4, Essence 1",
+    preReq: "none",
+    essenceCost: 3,
+    willpowerCost: 0,
+    healthLevelCost: 0,
+    ability: "none",
+    attribute: "none",
+    perfect: "false",
+    effect: "nullify penalties to ranged attack"
+  },
+  {
+    title: "Accuracy Without Distance",
+    statMin: "Archery 5, Essence 3",
+    preReq: "There is No Wind",
+    essenceCost: 4,
+    willpowerCost: 1,
+    healthLevelCost: 0,
+    ability: "Dexterity",
+    attribute: "Archery",
+    perfect: "false",
+    effect: "Ranged attack hits a valid target, soak and hardness still apply to damage"
+  },
+  {
+    title: "Forceful Arrow",
+    statMin: "Archery 4, Essence 2",
+    preReq: "There is No Wind",
+    essenceCost: 1,
+    willpowerCost: 0,
+    healthLevelCost: 0,
+    ability: "none",
+    attribute: "none",
+    perfect: "false",
+    effect: "a target hit by the attack, is pushed back (Essence * 3) yards if they take damage."
+  },
+  {
+    title: "Trance of Unhesitating Speed",
+    statMin: "Archery 3, Essence 2",
+    preReq: "Archery Excellency",
+    essenceCost: 2,
+    willpowerCost: 0,
+    healthLevelCost: 0,
+    ability: "Dexterity",
+    attribute: "Archery",
+    perfect: "false",
+    effect: "convert 1 attack, into a flurry of (Essence + 1) attacks with no multiple attack penalties."
+  },
+  {
+    title: "Arrow Storm Technique",
+    statMin: "Archery 5, Essence 2",
+    preReq: "Trance of Unhesitating Speed",
+    essenceCost: 5,
+    willpowerCost: 1,
+    healthLevelCost: 0,
+    ability: "Dexterity",
+    attribute: "Archery",
+    perfect: "false",
+    effect: "1 attack is rolled to hit (Essence x 3) targets, but never a single target or human sized target more than once."
+  },
+  {
+    title: "Flashing Vengeance Draw",
+    statMin: "Archery 3, Essence 2",
+    preReq: "Trance of Unhesitating Speed",
+    essenceCost: 3,
+    willpowerCost: 0,
+    healthLevelCost: 0,
+    ability: "none",
+    attribute: "none",
+    perfect: "false",
+    effect: "add Essence in autosuccesses to Join Battle, if first in line the first attack is made with 'unexpected attack' bonuses."
+  },
+  {
+    title: "x",
+    preReq: "y",
+    essenceCost: "e",
+    willpowerCost: "w",
+    healthLevelCost: "h",
+    ability: "aB",
+    attribute: "aT",
+    perfect: "true/false"
+  },
+  {
+    title: "x",
+    preReq: "y",
+    essenceCost: "e",
+    willpowerCost: "w",
+    healthLevelCost: "h",
+    ability: "aB",
+    attribute: "aT",
+    perfect: "true/false"
+  },
+  {
+    title: "x",
+    preReq: "y",
+    essenceCost: "e",
+    willpowerCost: "w",
+    healthLevelCost: "h",
+    ability: "aB",
+    attribute: "aT",
+    perfect: "true/false"
+  },
+  {
+    title: "x",
+    preReq: "y",
+    essenceCost: "e",
+    willpowerCost: "w",
+    healthLevelCost: "h",
+    ability: "aB",
+    attribute: "aT",
+    perfect: "true/false"
+  }
+]
